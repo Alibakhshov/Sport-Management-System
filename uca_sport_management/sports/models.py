229dd77310b1fe409e2af38d777c6b196ba6f277
Rauf Alibakhshov <rauf.alibakhshov_2025@ucentralasia.org>
2023-04-21 13:56:38 +0600

    Updates

--- uca_sport_management/sports/models.py
+++ uca_sport_management/sports/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     faculty = models.CharField(max_length=255)
     birth_date = models.DateField()
     email = models.EmailField(unique=True)
+    course = models.CharField(max_length=255)
     
     def __str__(self):
         return self.first_name + ' ' + self.last_name
