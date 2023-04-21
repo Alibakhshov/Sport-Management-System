69e7819667688dd4393e20042a85baff95adf89f
Rauf Alibakhshov <rauf.alibakhshov_2025@ucentralasia.org>
2023-04-21 10:31:28 +0600

    Updates

--- uca_sport_management/sports/views.py
+++ uca_sport_management/sports/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Player
 
-# Create your views here.
+def player_list(request):
+    players = Player.objects.all()
+    context = {'players': players}
+    return render(request, 'player/player_list.html', context)
+
